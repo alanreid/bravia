96fff4989ad9366c8f0f83f6e5297ab8a9fddb0e
Alan Gordon Reid <mail@alanreid.com.ar>
2017-01-24 11:00:17 +0100

    Adds back the demo.js file

---
+++ demo.js
@@ -0,0 +1,14 @@
+
+// Accepts two parameters: IP and PSKKey
+
+bravia('192.168.1.100', '0000', function(client) {
+
+  // List available commands
+  client.getCommandNames(function(list) {
+    console.log(list);
+  });
+
+  // Call a command
+  client.exec('Netflix');
+
+});
